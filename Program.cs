48d985ccc572e06009f3203166ff6eb3d9b64175
26EAR01 Estefany Albizurez <albizurezestefany16db@gmail.com>
2026-04-13 12:23:50 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Clasificacion de letras por rango");
+        Console.Write("Dame una letra");
+        char letra= Convert.ToChar(Console.ReadLine().ToLower());
+
+        switch (letra) 
+        { 
+            case 'A':
+                Console.WriteLine("Excelente");
+                break;
+            case 'B':
+                Console.WriteLine("Bueno");
+                break;
+            case 'C':
+                Console.WriteLine("Regular");
+                break;
+            case 'D':
+                Console.WriteLine("Deficiente");
+                break;
+            case 'F':
+                Console.WriteLine("Reprobado");
+                break;
+            default:
+                Console.WriteLine("Digite una letra entre A,B,C,D,F");
+                break;
+   
+        }
+    }
+}
